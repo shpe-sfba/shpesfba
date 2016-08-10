c683318977ad07b7265e2d3102c4f3fbbfa21d79
VeryBueno <rt90e78@gmail.com>
2016-08-09 19:32:34 -0700

    update static files config

--- shpesfbasite/settings.py
+++ shpesfbasite/settings.py
@@ -116,7 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.join(os.path.abspath(__file__), '../../'))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
