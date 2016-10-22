665477e446a16113cf486803c5a3d96d2bcd412d
Luis <lcuellar@calpoly.edu>
2016-10-22 15:34:12 -0700

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ rsync -rvh --exclude 'deploy.sh' --exclude '.env' --exclude '.DS_Store' --exclud
 ssh luis@205.185.120.249 <<'ENDSSH'
 source /opt/django/shpesfba/shpesfba/.env
 source /opt/django/shpesfba/venv/bin/activate
+pip install -r /opt/django/shpesfba/shpesfba/requirements.txt
 python /opt/django/shpesfba/shpesfba/manage.py collectstatic --no-input
 python /opt/django/shpesfba/shpesfba/manage.py migrate
 touch /opt/django/shpesfba/uwsgi.ini
